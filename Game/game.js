ca8efa757803752cd658b45ff38caca8bd646319
Anastasia <anastasiapan93@gmail.com>
2024-03-21 19:02:39 -0700

    first commit

---
+++ Game/game.js
@@ -0,0 +1,24 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+
+  export default Game;
+  
+  class GameSavingData {
+  }
+
+  export class GameSavingData {}
+  
+  function readGameSaving() {
+  }
+
+  export function readGameSaving() {}
+  
+  function writeGameSaving() {
+  }
+
+  export function writeGameSaving() {}
+
+  import Character, {Character} from './domain.js';
